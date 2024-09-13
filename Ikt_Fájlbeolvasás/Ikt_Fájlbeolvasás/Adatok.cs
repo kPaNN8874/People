d1a1ecaccf877e59cc5aef0e161399524f381509
kPaNN8874 <113461676+kPaNN8874@users.noreply.github.com>
2024-09-13 13:09:03 +0200

    Add files via upload

---
+++ Ikt_Fájlbeolvasás/Ikt_Fájlbeolvasás/Adatok.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ikt_Fájlbeolvasás
+{
+    internal class Adatok
+    {
+        public string szam;
+        public string keresztnev;
+        public string vezeteknev;
+
+        public string Szam { get => szam; set => szam = value; }
+        public string Keresztnev { get => keresztnev; set => keresztnev = value; }
+        public string Vezeteknev { get => vezeteknev; set => vezeteknev = value; }
+
+        public Adatok(string szam, string keresztnev, string vezeteknev)
+        {
+            this.Szam = szam;
+            this.Keresztnev = keresztnev;
+            this.Vezeteknev = vezeteknev;
+        }
+    }
+    
+
+}
---
+++ Ikt_Fájlbeolvasás/Ikt_Fájlbeolvasás/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Reflection.Emit;
+using System.Linq;
+namespace Ikt_Fájlbeolvasás
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<string> list = new();
+            using (StreamReader sr = new StreamReader("people.csv"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
